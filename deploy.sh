96bc05595c53c95484618c8eecb292e57cd210d4
Lance Fogle <lancefogle@users.noreply.github.com>
2017-12-27 18:33:22 -0500

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-modsecurityBranch=v3/master
 crsVersion=3.0.2
 
 # Create modules link if doesn't exist
@@ -14,32 +13,27 @@ cp -f ./ngx_http_modsecurity_module.so /etc/nginx/modules/
 # Create modsec directory if it doesn't exist
 if [ ! -d /etc/nginx/modsec ]; then
 	mkdir /etc/nginx/modsec
-	cp ./main.conf /etc/nginx/modsec/
 fi
 
 # Install custom modsecurity config for nginx
-if [ ! -f /etc/nginx/modsec/main.conf ]; then
-	cp ./main.conf /etc/nginx/modsec/
-else
-	cp ./main.conf /etc/nginx/modsec/main.conf.new
-fi
+wget -q -O /etc/nginx/modsec/main.conf.new https://raw.githubusercontent.com/OnyxfireInc/modsecurity-nginx/master/main.conf
 
-# Install default config
-if [ ! -f /etc/nginx/conf.d/default.conf ]; then
-	cp ./default.conf /etc/nginx/conf.d/
-else
-	cp ./default.conf /etc/nginx/conf.d/default.conf.new
-fi
+# Install nginx base configuration
+wget -q -O /etc/nginx/nginx.conf.new https://raw.githubusercontent.com/OnyxfireInc/modsecurity-nginx/master/nginx.conf
 
-# Install libmodsecurity
-if [ ! -d /usr/local/modsecurity/ ]; then
-	tar -zxvf ./modsecurity.tar.gz -C /usr/local/modsecurity/
-	find /usr/local/modsecurity/ -type d -exec chmod 0755 {} \;
-	find /usr/local/modsecurity/ -type f -exec chmod 0644 {} \;
-	chmod 0755 /usr/local/modsecurity/lib/libmodsecurity.la /usr/local/modsecurity/lib/libmodsecurity.so.3.0.0
-	ln -sf /usr/local/modsecurity/lib/libmodsecurity.so.3.0.0 /usr/local/modsecurity/lib/libmodsecurity.so
-	ln -sf /usr/local/modsecurity/lib/libmodsecurity.so.3.0.0 /usr/local/modsecurity/lib/libmodsecurity.so.3
+# Install default server config
+wget -q -O /etc/nginx/conf.d/default.conf.new https://raw.githubusercontent.com/OnyxfireInc/modsecurity-nginx/master/default.conf
+
+# Install or update installed libmodsecurity
+if [ -d /usr/local/modsecurity/ ]; then
+	rm -rf /usr/local/modsecurity/
 fi
+wget -q -O - https://raw.githubusercontent.com/OnyxfireInc/modsecurity-nginx/master/modsecurity.tar.gz | tar -zx -C /usr/local
+find /usr/local/modsecurity/ -type d -exec chmod 0755 {} \;
+find /usr/local/modsecurity/ -type f -exec chmod 0644 {} \;
+chmod 0755 /usr/local/modsecurity /usr/local/modsecurity/lib/libmodsecurity.la /usr/local/modsecurity/lib/libmodsecurity.so.3.0.0
+ln -sf /usr/local/modsecurity/lib/libmodsecurity.so.3.0.0 /usr/local/modsecurity/lib/libmodsecurity.so
+ln -sf /usr/local/modsecurity/lib/libmodsecurity.so.3.0.0 /usr/local/modsecurity/lib/libmodsecurity.so.3
 
 # Remove existing OWASP CRS rules
 if [ -d /etc/nginx/modsec/crs ]; then
@@ -47,12 +41,12 @@ if [ -d /etc/nginx/modsec/crs ]; then
 fi
 
 # Install OWASP CRS rules
-wget https://github.com/SpiderLabs/owasp-modsecurity-crs/archive/v${crsVersion}.tar.gz
-tar -zxf v${crsVersion}.tar.gz -C /etc/nginx/modsec/
+wget -q -O - https://github.com/SpiderLabs/owasp-modsecurity-crs/archive/v${crsVersion}.tar.gz | tar zx -C /etc/nginx/modsec/
 mv /etc/nginx/modsec/owasp-modsecurity-crs-${crsVersion} /etc/nginx/modsec/crs
+cp /etc/nginx/modsec/crs/crs-setup.conf.example /etc/nginx/modsec/crs/crs/crs-setup.conf
 
 # Install recommended modsecurity config
-wget -P /etc/nginx/modsec/ https://raw.githubusercontent.com/SpiderLabs/ModSecurity/${modsecurityBranch}/modsecurity.conf-recommended
+wget -q -P /etc/nginx/modsec/ https://raw.githubusercontent.com/SpiderLabs/ModSecurity/v3/master/modsecurity.conf-recommended
 if [ ! -f /etc/nginx/modsec/modsecurity.conf ]; then
 	mv /etc/nginx/modsec/modsecurity.conf-recommended /etc/nginx/modsec/modsecurity.conf
 	sed -i 's/SecRuleEngine DetectionOnly/SecRuleEngine On/' /etc/nginx/modsec/modsecurity.conf
